28c0904c9a65f8a695a6b691b3325bb9ab3880e4
Aditya Dharmawan Saputra <adityadees@gmail.com>
2023-01-30 02:48:50 +0700

    Update Layanan.php

--- application/controllers/api/Layanan.php
+++ application/controllers/api/Layanan.php
@@ -18,6 +18,7 @@ class Layanan extends REST_Controller
     public function index_post()
     {
         $nama = $this->security->xss_clean($this->post("nama"));
+        $icon = $this->security->xss_clean($this->post("icon"));
         $this->form_validation->set_rules("nama", "Nama", "required");
         if ($this->form_validation->run() === FALSE) {
             return $this->response([
@@ -26,51 +27,20 @@ class Layanan extends REST_Controller
             ], REST_Controller::HTTP_BAD_REQUEST);
         } else {
             if (!empty($nama)) {
-                if (!empty($_FILES['icon_layanan']['name'])) {
-                    $files = $_FILES;
-                    $dir = realpath(APPPATH . '../assets/uploads/icon_layanan');
+                $layanan = array(
+                    "nama" => $nama,
+                    "icon" => $icon,
+                );
 
-                    $_FILES['icon_layanan']['name'] = $files['icon_layanan']['name'];
-                    $_FILES['icon_layanan']['type'] = $files['icon_layanan']['type'];
-                    $_FILES['icon_layanan']['tmp_name'] = $files['icon_layanan']['tmp_name'];
-                    $_FILES['icon_layanan']['error'] = $files['icon_layanan']['error'];
-                    $_FILES['icon_layanan']['size'] = $files['icon_layanan']['size'];
-
-                    $config['upload_path']          = $dir;
-                    $config['allowed_types']        = 'gif|jpg|jpeg|png';
-                    $config['max_size']             = 1024 * 10;
-
-                    $this->load->library('upload', $config);
-                    $this->upload->initialize($config);
-                    if (!$this->upload->do_upload('icon_layanan')) {
-                        $error = array('error' => $this->upload->display_errors());
-                        $data = array(
-                            "status"    => "Gagal",
-                            "pesan"     => $error,
-                        );
-                    } else {
-                        $upload_data = $this->upload->data();
-                        $layanan = array(
-                            "nama" => $nama,
-                            "icon" => $upload_data['file_name'],
-                        );
-
-                        if ($this->layanan_model->insert_layanan($layanan)) {
-                            return $this->response([
-                                'status' => "Sukses",
-                                'message' => 'Data Berhasil Ditambah',
-                            ], REST_Controller::HTTP_OK);
-                        } else {
-                            return $this->response([
-                                'status' => "Error",
-                                'message' => 'Data Gagal Ditambah',
-                            ], REST_Controller::HTTP_BAD_REQUEST);
-                        }
-                    }
+                if ($this->layanan_model->insert_layanan($layanan)) {
+                    return $this->response([
+                        'status' => "Sukses",
+                        'message' => 'Data Berhasil Ditambah',
+                    ], REST_Controller::HTTP_OK);
                 } else {
                     return $this->response([
-                        'status' => "Gagal",
-                        'message' => 'Bukti Pembayaran Harus Disertakan',
+                        'status' => "Error",
+                        'message' => 'Data Gagal Ditambah',
                     ], REST_Controller::HTTP_BAD_REQUEST);
                 }
             } else {
