ccb6d5c98f012638a191144f1230fdee9bcb3e8f
shaffan94 <30769525+shaffan94@users.noreply.github.com>
2023-03-09 05:14:53 +0700

    udpate api company profile status

--- application/controllers/api/Company_profile_status.php
+++ application/controllers/api/Company_profile_status.php
@@ -17,17 +17,17 @@ class Company_profile_status extends REST_Controller
 
     public function index_post()
     {
-        $cp_nama = $this->security->xss_clean($this->post("cp_nama"));
-        $this->form_validation->set_rules("cp_nama", "Cp_nama", "required");
+        $cps_nama = $this->security->xss_clean($this->post("cps_nama"));
+        $this->form_validation->set_rules("cps_nama", "Cps_nama", "required");
         if ($this->form_validation->run() === FALSE) {
             $this->response([
                 'status' => "Error",
                 'message' => 'Data Gagal Ditambah',
             ], REST_Controller::HTTP_BAD_REQUEST);
         } else {
-            if (!empty($cp_nama)) {
+            if (!empty($cps_nama)) {
                 $cp_status = array(
-                    "cp_nama" => $cp_nama,
+                    "cps_nama" => $cps_nama,
                 );
 
                 if ($this->company_profile_status_model->insert_company_profile_status($cp_status)) {
@@ -47,6 +47,63 @@ class Company_profile_status extends REST_Controller
                     'message' => 'Data Gagal Ditambah',
                 ], REST_Controller::HTTP_BAD_REQUEST);
             }
+        }
+    }
+
+    public function index_put()
+    {
+        $id = $this->security->xss_clean($this->put("id"));
+        $cps_nama = $this->security->xss_clean($this->put("cps_nama"));
+
+        if (!empty($id) && !empty($cps_nama)) {
+            $cp_status = array(
+                "cps_nama" => $cps_nama,
+            );
+
+            if ($this->company_profile_status_model->update_company_profile_status($id, $cp_status)) {
+                $this->response([
+                    'status' => "Success",
+                    'message' => 'Data Berhasil Diupdate',
+                ], REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => "Gagal",
+                    'message' => 'Data Gagal Diupdate',
+                ], REST_Controller::HTTP_OK);
+            }
+        } else {
+            $this->response([
+                'status' => "Error",
+                'message' => 'Data Gagal Diupdate',
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function index_delete()
+    {
+        $id = $this->security->xss_clean($this->delete("id"));
+
+        if (!empty($id)) {
+            $cp_status = array(
+                "status" => "dihapus",
+            );
+
+            if ($this->company_profile_status_model->update_company_profile_status($id, $cp_status)) {
+                $this->response([
+                    'status' => "Success",
+                    'message' => 'Data Berhasil Dihapus',
+                ], REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => "Gagal",
+                    'message' => 'Data Gagal Dihapus',
+                ], REST_Controller::HTTP_OK);
+            }
+        } else {
+            $this->response([
+                'status' => "Error",
+                'message' => 'Data Gagal Dihapus',
+            ], REST_Controller::HTTP_BAD_REQUEST);
         }
     }
 
--- application/models/api/Company_profile_status_model.php
+++ application/models/api/Company_profile_status_model.php
@@ -12,6 +12,7 @@ class Company_profile_status_model extends CI_Model
     {
         $this->db->select("*");
         $this->db->from("company_profile_status");
+        $this->db->where("status", "ditampilkan");
         $query = $this->db->get();
 
         return $query->result();
@@ -20,5 +21,11 @@ class Company_profile_status_model extends CI_Model
     public function insert_company_profile_status($data = [])
     {
         return $this->db->insert("company_profile_status", $data);
+    }
+
+    public function update_company_profile_status($id, $data)
+    {
+        $this->db->where("cps_id", $id);
+        return $this->db->update("company_profile_status", $data);
     }
 }
